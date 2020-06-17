1ba2951f37049e1d6214008ee380c4982eef7fff
Maxim <MaxVMH@GitHub>
2020-06-17 13:23:52 +0200

    Added DifficultyMode and makeFrequencyArray

---
+++ Audio-Frequency-Trainer/DifficultyMode.swift
@@ -0,0 +1,13 @@
+//
+//  DifficultyMode.swift
+//  Audio-Frequency-Trainer
+//
+//  Created by Maxim Van Meerhaeghe on 17/06/2020.
+//  Copyright © 2020 Mixam. All rights reserved.
+//
+
+import Foundation
+
+enum DifficultyMode {
+    case easy, normal, hard, pro
+}
--- Audio-Frequency-Trainer/Frequency.swift
+++ Audio-Frequency-Trainer/Frequency.swift
@@ -9,11 +9,78 @@
 import Foundation
 
 struct Frequency {
-    let value: Int
+    let frequency: Int
     var description: String {
-        if value > 1000 {
-            return "\(Double(value)/1000) kHz"
+        if frequency > 1000 {
+            return "\(Double(frequency)/1000) kHz"
         }
-        return "\(value) Hz"
+        return "\(frequency) Hz"
+    }
+}
+
+func makeFrequencyArray(difficultyMode: DifficultyMode) -> [Frequency] {
+    var frequencies = [Frequency]()
+    switch difficultyMode {
+    case .easy:
+        frequencies += [Frequency(frequency: 250)]
+        frequencies += [Frequency(frequency: 800)]
+        frequencies += [Frequency(frequency: 2500)]
+        frequencies += [Frequency(frequency: 8000)]
+        return frequencies
+    case .normal:
+        frequencies += [Frequency(frequency: 100)]
+        frequencies += [Frequency(frequency: 200)]
+        frequencies += [Frequency(frequency: 400)]
+        frequencies += [Frequency(frequency: 800)]
+        frequencies += [Frequency(frequency: 1600)]
+        frequencies += [Frequency(frequency: 3150)]
+        frequencies += [Frequency(frequency: 6300)]
+        frequencies += [Frequency(frequency: 12500)]
+        return frequencies
+    case .hard:
+        frequencies += [Frequency(frequency: 80)]
+        frequencies += [Frequency(frequency: 125)]
+        frequencies += [Frequency(frequency: 160)]
+        frequencies += [Frequency(frequency: 250)]
+        frequencies += [Frequency(frequency: 315)]
+        frequencies += [Frequency(frequency: 500)]
+        frequencies += [Frequency(frequency: 630)]
+        frequencies += [Frequency(frequency: 1000)]
+        frequencies += [Frequency(frequency: 1250)]
+        frequencies += [Frequency(frequency: 2000)]
+        frequencies += [Frequency(frequency: 2500)]
+        frequencies += [Frequency(frequency: 4000)]
+        frequencies += [Frequency(frequency: 5000)]
+        frequencies += [Frequency(frequency: 8000)]
+        frequencies += [Frequency(frequency: 10000)]
+        frequencies += [Frequency(frequency: 16000)]
+        return frequencies
+    case .pro:
+        frequencies += [Frequency(frequency: 63)]
+        frequencies += [Frequency(frequency: 80)]
+        frequencies += [Frequency(frequency: 100)]
+        frequencies += [Frequency(frequency: 125)]
+        frequencies += [Frequency(frequency: 160)]
+        frequencies += [Frequency(frequency: 200)]
+        frequencies += [Frequency(frequency: 250)]
+        frequencies += [Frequency(frequency: 315)]
+        frequencies += [Frequency(frequency: 400)]
+        frequencies += [Frequency(frequency: 500)]
+        frequencies += [Frequency(frequency: 630)]
+        frequencies += [Frequency(frequency: 800)]
+        frequencies += [Frequency(frequency: 1000)]
+        frequencies += [Frequency(frequency: 1250)]
+        frequencies += [Frequency(frequency: 1600)]
+        frequencies += [Frequency(frequency: 2000)]
+        frequencies += [Frequency(frequency: 2500)]
+        frequencies += [Frequency(frequency: 3150)]
+        frequencies += [Frequency(frequency: 4000)]
+        frequencies += [Frequency(frequency: 5000)]
+        frequencies += [Frequency(frequency: 6300)]
+        frequencies += [Frequency(frequency: 8000)]
+        frequencies += [Frequency(frequency: 10000)]
+        frequencies += [Frequency(frequency: 12500)]
+        frequencies += [Frequency(frequency: 16000)]
+        return frequencies
     }
 }
