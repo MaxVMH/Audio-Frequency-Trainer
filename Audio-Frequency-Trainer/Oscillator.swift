b8684f1c3ec3583358d4b5a6cdd4beb3ba478b8d
Maxim <MaxVMH@GitHub>
2020-06-17 14:05:51 +0200

    Added an Oscillator with basic functionality

---
+++ Audio-Frequency-Trainer/Oscillator.swift
@@ -0,0 +1,30 @@
+//
+//  Oscillator.swift
+//  Audio-Frequency-Trainer
+//
+//  Created by Maxim Van Meerhaeghe on 17/06/2020.
+//  Copyright © 2020 Mixam. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+class Oscillator {
+    var oscillator = AKOscillator()
+    
+    func start() {
+        AudioKit.output = oscillator
+        try? AudioKit.start()
+        oscillator.rampDuration = 0
+        oscillator.play()
+    }
+    func pause() {
+        oscillator.stop()
+    }
+    func changeFrequency(to value: Int) {
+        oscillator.frequency = Double(value)
+    }
+    func changeVolume(to value: Int) {
+        oscillator.amplitude = Double(value)
+    }
+}
