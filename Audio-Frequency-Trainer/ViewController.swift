36d08e70cec2de65da056533c35a98b8081dd169
Maxim <MaxVMH@GitHub>
2020-06-19 14:11:01 +0200

    Temporary alert to show the correct answer.

--- Audio-Frequency-Trainer/ViewController.swift
+++ Audio-Frequency-Trainer/ViewController.swift
@@ -29,6 +29,10 @@ class ViewController: UIViewController {
     }
     
     @IBAction func nextButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Maybe you were correct.", message: "The answer you are looking for is boobies and \(currentFrequency!.description)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
         currentFrequency = getNewFrequency(previousFrequency: currentFrequency!, allFrequencies: frequencies)
         oscillator.stop()
         oscillator.changeFrequency(to: currentFrequency!.frequency)
