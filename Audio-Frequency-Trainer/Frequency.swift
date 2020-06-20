a095950d9bb0b3ad045a84b46a034dc803bd8210
Maxim <MaxVMH@GitHub>
2020-06-20 13:56:22 +0200

    Included difficulty as a property of Frequency so we only have to make the frequency array once and filter later

--- Audio-Frequency-Trainer/Frequency.swift
+++ Audio-Frequency-Trainer/Frequency.swift
@@ -10,6 +10,7 @@ import Foundation
 
 struct Frequency: Equatable {
     let frequency: Int
+    let difficulty: DifficultyMode
     var description: String {
         if frequency >= 1000 {
             return "\(Double(frequency)/1000) kHz"
@@ -18,71 +19,40 @@ struct Frequency: Equatable {
     }
 }
 
-func makeFrequencyArray(difficultyMode: DifficultyMode) -> [Frequency] {
+func makeFrequencyArray() -> [Frequency] {
     var frequencies = [Frequency]()
-    switch difficultyMode {
-    case .easy:
-        frequencies += [Frequency(frequency: 250)]
-        frequencies += [Frequency(frequency: 800)]
-        frequencies += [Frequency(frequency: 2500)]
-        frequencies += [Frequency(frequency: 8000)]
-        return frequencies
-    case .normal:
-        frequencies += [Frequency(frequency: 100)]
-        frequencies += [Frequency(frequency: 200)]
-        frequencies += [Frequency(frequency: 400)]
-        frequencies += [Frequency(frequency: 800)]
-        frequencies += [Frequency(frequency: 1600)]
-        frequencies += [Frequency(frequency: 3150)]
-        frequencies += [Frequency(frequency: 6300)]
-        frequencies += [Frequency(frequency: 12500)]
-        return frequencies
-    case .hard:
-        frequencies += [Frequency(frequency: 80)]
-        frequencies += [Frequency(frequency: 125)]
-        frequencies += [Frequency(frequency: 160)]
-        frequencies += [Frequency(frequency: 250)]
-        frequencies += [Frequency(frequency: 315)]
-        frequencies += [Frequency(frequency: 500)]
-        frequencies += [Frequency(frequency: 630)]
-        frequencies += [Frequency(frequency: 1000)]
-        frequencies += [Frequency(frequency: 1250)]
-        frequencies += [Frequency(frequency: 2000)]
-        frequencies += [Frequency(frequency: 2500)]
-        frequencies += [Frequency(frequency: 4000)]
-        frequencies += [Frequency(frequency: 5000)]
-        frequencies += [Frequency(frequency: 8000)]
-        frequencies += [Frequency(frequency: 10000)]
-        frequencies += [Frequency(frequency: 16000)]
-        return frequencies
-    case .pro:
-        frequencies += [Frequency(frequency: 63)]
-        frequencies += [Frequency(frequency: 80)]
-        frequencies += [Frequency(frequency: 100)]
-        frequencies += [Frequency(frequency: 125)]
-        frequencies += [Frequency(frequency: 160)]
-        frequencies += [Frequency(frequency: 200)]
-        frequencies += [Frequency(frequency: 250)]
-        frequencies += [Frequency(frequency: 315)]
-        frequencies += [Frequency(frequency: 400)]
-        frequencies += [Frequency(frequency: 500)]
-        frequencies += [Frequency(frequency: 630)]
-        frequencies += [Frequency(frequency: 800)]
-        frequencies += [Frequency(frequency: 1000)]
-        frequencies += [Frequency(frequency: 1250)]
-        frequencies += [Frequency(frequency: 1600)]
-        frequencies += [Frequency(frequency: 2000)]
-        frequencies += [Frequency(frequency: 2500)]
-        frequencies += [Frequency(frequency: 3150)]
-        frequencies += [Frequency(frequency: 4000)]
-        frequencies += [Frequency(frequency: 5000)]
-        frequencies += [Frequency(frequency: 6300)]
-        frequencies += [Frequency(frequency: 8000)]
-        frequencies += [Frequency(frequency: 10000)]
-        frequencies += [Frequency(frequency: 12500)]
-        frequencies += [Frequency(frequency: 16000)]
-        return frequencies
-    }
+    frequencies += [Frequency(frequency: 20, difficulty: .pro)]
+    frequencies += [Frequency(frequency: 25, difficulty: .pro)]
+    frequencies += [Frequency(frequency: 32, difficulty: .pro)]
+    frequencies += [Frequency(frequency: 40, difficulty: .pro)]
+    frequencies += [Frequency(frequency: 50, difficulty: .pro)]
+    frequencies += [Frequency(frequency: 63, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 80, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 100, difficulty: .normal)]
+    frequencies += [Frequency(frequency: 125, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 160, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 200, difficulty: .easy)]
+    frequencies += [Frequency(frequency: 250, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 315, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 400, difficulty: .normal)]
+    frequencies += [Frequency(frequency: 500, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 630, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 800, difficulty: .easy)]
+    frequencies += [Frequency(frequency: 1000, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 1250, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 1600, difficulty: .normal)]
+    frequencies += [Frequency(frequency: 2000, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 2500, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 3150, difficulty: .easy)]
+    frequencies += [Frequency(frequency: 4000, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 5000, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 6300, difficulty: .normal)]
+    frequencies += [Frequency(frequency: 8000, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 10000, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 12500, difficulty: .easy)]
+    frequencies += [Frequency(frequency: 16000, difficulty: .hard)]
+    frequencies += [Frequency(frequency: 20000, difficulty: .pro)]
+    return frequencies
 }
 
 func getNewFrequency(previousFrequency: Frequency, allFrequencies: [Frequency]) -> Frequency {
